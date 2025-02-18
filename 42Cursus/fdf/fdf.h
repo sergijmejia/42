5550d96683ae72a80122f069df8a809652b00270
Sergio Mejia Assmus <smejia-a@car15s3.42barcelona.com>
2025-02-18 17:03:23 +0100

    fdf v0.2

--- 42Cursus/fdf/fdf.h
+++ 42Cursus/fdf/fdf.h
@@ -6,7 +6,7 @@
 /*   By: smejia-a <smejia-a@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 14:16:24 by smejia-a          #+#    #+#             */
-/*   Updated: 2025/02/17 14:20:00 by smejia-a         ###   ########.fr       */
+/*   Updated: 2025/02/18 16:39:04 by smejia-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,25 @@
 # include <./MLX42/include//MLX42/MLX42.h>
 # include "./libft/libft.h"
 
+typedef struct s_window
+{
+	int	width;
+	int	height;
+}	t_window;
+
+typedef struct s_pixel_data
+{
+	int	x;
+	int	y;
+	int	z;
+}	t_pixel_data;
+
+# ifndef WIDTH
+#  define WIDTH 3840
+# endif
+
+# ifndef HEIGHT
+#  define HEIGHT 2160
+# endif
 
 #endif
--- 42Cursus/fdf/src/fdf.c
+++ 42Cursus/fdf/src/fdf.c
@@ -6,21 +6,92 @@
 /*   By: smejia-a <smejia-a@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 14:15:34 by smejia-a          #+#    #+#             */
-/*   Updated: 2025/02/17 17:03:07 by smejia-a         ###   ########.fr       */
+/*   Updated: 2025/02/18 17:00:43 by smejia-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fdf.h>
 
-
-int	main()
+/*Funcion de libearacion de memoria de la matriz*/
+t_pixel_data	*ft_clean_matrix(t_pixel_data **pixel, int width, int height)
 {
-	void	*mlx;
-	void	*win;
+	int	i;
 
-	mlx = mlx_init();
-	win = mlx_new_window(mlx, 800, 600, "Fdf");
+	i = 0;
+	while (i < height)
+	{
+		free(pixel[i]);
+		pixel[i] = NULL;
+		i++;
+	}
+	free(pixel);
+	pixel = NULL;
+	return (NULL);
+}
+
+t_pixel_data	**create_matrix(int fd)
+{
+
+}
+
+void	design_square(mlx_image_t *img)
+{
+	int	x;
+	int	y;
+	int	index;
+
+	y = 50;
+	while (y < 150)
+	{
+		x = 50;
+		while (x < 150)
+		{
+			index = (y * img->width + x) * 4;
+			img->pixels[index] = 255; //rojo
+			img->pixels[index + 1] = 0; //verde
+			img->pixels[index + 2] = 0; //azul
+			img->pixels[index + 3] = 255; //alpha
+			x++;
+		}
+		y++;
+	}
+}
+
+void	ft_key_hook(mlx_key_data_t key_hook, void *param)
+{
+	mlx_t	*mlx;
+
+	mlx = (mlx_t*)param;
+	if (key_hook.key == MLX_KEY_ESCAPE)
+	{
+		mlx_terminate(mlx);
+		exit(EXIT_SUCCESS);
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	mlx_t	*mlx;
+	mlx_image_t	*img;
+	t_pixel_data	**pixel;
+
+	if (argc != 2)
+		return (EXIT_FAILURE);
+	fd = open(argv[1], O_RDONLY);
+	pixel = create_matrix(fd);
+	mlx = mlx_init(WIDTH, HEIGHT, "My MLX42", true);
+	if (mlx == NULL)
+		exit(EXIT_FAILURE);
+	img = mlx_new_image(mlx, 200, 200);
+	if (!img)
+	{
+		mlx_terminate(mlx);
+		exit(EXIT_FAILURE);
+	}
+	design_square(img);
+	mlx_image_to_window(mlx, img, 100, 100);
+	mlx_key_hook(mlx, ft_key_hook, mlx);
 	mlx_loop(mlx);
-
-	return (0);
+	mlx_terminate(mlx); //libera memoria antes de salir
+	return (EXIT_SUCCESS);
 }
