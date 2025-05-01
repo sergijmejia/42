37a0508fc2710d1b07e2c647ec21b8ba85cde0df
Sergio Mejia Assmus <smejia-a@car4s1.42barcelona.com>
2025-05-01 19:29:43 +0200

    philosophers v0.2

--- 42Cursus/philosophers/philo/philosophers.h
+++ 42Cursus/philosophers/philo/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: smejia-a <smejia-a@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 18:33:36 by smejia-a          #+#    #+#             */
-/*   Updated: 2025/04/25 18:34:09 by smejia-a         ###   ########.fr       */
+/*   Updated: 2025/05/01 18:12:56 by smejia-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,31 @@
 # include <sys/time.h>
 # include <pthread.h>
 
-typedef struct s_philo_data
+typedef struct	s_philo_data
 {
-	int	number_of_philo;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
-	int	number_to_eat;
+	int				number_of_philo;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				number_to_eat;
 } t_philo_data;
 
-typedef struct s_philo
+typedef struct	s_waiter
 {
-	int n_philosopher;
-	int time_last_eat;
-	int	state;
+	int				id_next_eat;
+} t_waiter;
+
+typedef struct	s_philo
+{
+	pthread_t		thread;
+	int				id;
+	long long		time_last_eat;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
+	int				state;
+	int				is_alive;
+	t_waiter		*waiter;
+	t_philo_data	data;
 } t_philo;
 
 #endif
--- 42Cursus/philosophers/philo/src/philosophers.c
+++ 42Cursus/philosophers/philo/src/philosophers.c
@@ -6,11 +6,11 @@
 /*   By: smejia-a <smejia-a@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 19:39:01 by smejia-a          #+#    #+#             */
-/*   Updated: 2025/04/25 18:34:06 by smejia-a         ###   ########.fr       */
+/*   Updated: 2025/05/01 19:24:56 by smejia-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <philosophers.h>
+#include "philosophers.h"
 
 int	ft_atoi(char *nptr)
 {
@@ -58,14 +58,260 @@ int	check_values(int argc, char **argv)
 	return (0);
 }
 
+void	print_dead(int id, long long time)
+{
+	printf("%lld %d died\n", time, id);
+}
+
+void    print_start_thinking(int id, long long time)
+{
+	printf("%lld %d is thinking\n", time, id);
+}
+
+void    print_get_fork(int id, long long time)
+{
+	printf("%lld %d has taken a fork\n", time, id);
+}
+
+void    print_start_eat(int id, long long time)
+{
+	printf("%lld %d is eating\n", time, id);
+}
+
+void	print_start_sleeping(int id, long long time)
+{
+	printf("%lld %d is sleeping\n", time, id);
+}
+
+long long	check_alive(t_philo *philosophers)
+{
+	int				time_to_die;
+	struct timeval	actual_tv;
+	long long		actual_time;
+
+	time_to_die = philosophers->data.time_to_die;
+	gettimeofday(&actual_tv, NULL);
+	actual_time = actual_tv.tv_sec * 1000 + actual_tv.tv_usec / 1000;
+	if (((long long)time_to_die) < (actual_time - philosophers->time_last_eat))
+		return (-1);
+	else
+		return (actual_time);
+}
+
+long long	check_sleep_time(t_philo *philosophers, long long time, int sleep)
+{
+	long long	last_eat;
+	int			time_to_die;
+	useconds_t	sleep_to_die;
+
+	last_eat = philosophers->time_last_eat;
+	time_to_die = philosophers->data.time_to_die;
+	if ((last_eat + (long long) time_to_die) <= (time + (long long) sleep))
+	{
+		sleep_to_die = (useconds_t) (last_eat + (long long) time_to_die - time);
+		usleep(sleep_to_die);
+		return (last_eat + (long long) time_to_die);
+	}
+	else
+		return (0);
+}
+
+void	next_allowed_philosopher(t_philo *philosophers)
+{
+	philosophers->waiter->id_next_eat++;
+	if (philosophers->waiter->id_next_eat > philosophers->data.number_of_philo)
+		philosophers->waiter->id_next_eat = 1;
+	printf("El siguiente que puede comer es: %d\n", philosophers->waiter->id_next_eat);
+	return ;
+}
+
+void	philo_think(t_philo *philosophers)
+{
+	long long		actual_time;
+
+	actual_time = check_alive(philosophers);
+	if (actual_time == -1)
+	{
+		print_dead(philosophers->id, actual_time);
+		philosophers->is_alive = 0;
+		return ;
+	}
+	else
+	{
+		print_start_thinking(philosophers->id, actual_time);
+		philosophers->state = 1;
+		return ;
+	}
+}
+
+void	philo_get_right_fork(t_philo *philosophers)
+{
+	int			id_forbidden;
+	long long	actual_time;
+
+	actual_time = check_alive(philosophers);
+    if (actual_time == -1)
+	{
+		print_dead(philosophers->id, actual_time);
+		philosophers->is_alive = 0;
+		return ;
+	}
+	id_forbidden = philosophers->waiter->id_next_eat - 1;
+	if (id_forbidden == 0)
+		id_forbidden = philosophers->data.number_of_philo;
+	if (philosophers->id == id_forbidden)
+		return ;
+	else
+	{
+		print_get_fork(philosophers->id, actual_time);
+		pthread_mutex_lock(philosophers->right_fork);
+		philosophers->state = 2;
+		return ;
+	}
+}
+
+void	philo_get_left_fork_and_eat(t_philo *philosophers)
+{
+	int			id_allowed;
+	long long	actual_time;
+	useconds_t	time_to_eat;
+
+	actual_time = check_alive(philosophers);
+	if (actual_time == -1)
+	{
+		print_dead(philosophers->id, actual_time);
+		philosophers->is_alive = 0;
+		return ;
+	}
+	time_to_eat = (useconds_t) philosophers->data.time_to_eat;
+	id_allowed = philosophers->waiter->id_next_eat;
+	if (philosophers->id == id_allowed)
+	{
+		print_get_fork(philosophers->id, actual_time);
+		print_start_eat(philosophers->id, actual_time);
+		pthread_mutex_lock(philosophers->left_fork);
+		//printf("El tiempo de comer es: %lld\n", (long long) time_to_eat);
+		usleep(time_to_eat);
+		//printf("Termina de comer\n");
+		pthread_mutex_unlock(philosophers->left_fork);
+		pthread_mutex_unlock(philosophers->right_fork);
+		next_allowed_philosopher(philosophers);
+		philosophers->state = 3;
+		return ;
+	}
+	else
+		return ;
+}
+
+void	philo_get_sleep(t_philo *philosophers)
+{
+	long long	actual_time;
+	useconds_t	time_to_sleep;
+	long long	dead_time;
+
+	actual_time = check_alive(philosophers);
+	if (actual_time == -1)
+	{
+		print_dead(philosophers->id, actual_time);
+		philosophers->is_alive = 0;
+		return ;
+	}
+	time_to_sleep = (useconds_t) philosophers->data.time_to_sleep;
+	print_start_sleeping(philosophers->id, actual_time);
+	dead_time = check_sleep_time(philosophers, actual_time, time_to_sleep);
+	if (dead_time > 0)
+	{
+		print_dead(philosophers->id, dead_time);
+		philosophers->is_alive = 0;
+		return ;
+	}
+	usleep(time_to_sleep);
+	philosophers->state = 0;
+	return ;
+}
+
+void	*routine(void *arg)
+{
+	t_philo		*philosophers;
+	useconds_t	time_to_sleep;
+	useconds_t	time_to_eat;
+
+	philosophers = (t_philo *) arg;
+	time_to_sleep = (useconds_t) philosophers->data.time_to_sleep;
+	time_to_eat = (useconds_t) philosophers->data.time_to_eat;
+	while (philosophers->is_alive)
+	{
+		if (philosophers->state == 0)
+			philo_think(philosophers);
+		else if (philosophers->state == 1)
+			philo_get_right_fork(philosophers);
+		else if (philosophers->state == 2)
+			philo_get_left_fork_and_eat(philosophers);
+		else if (philosophers->state == 3)
+			philo_get_sleep(philosophers);
+	}
+	printf("Sale de la rutina\n");
+	pthread_detach(philosophers->thread);
+	return (NULL);
+}
+
+void	philosophers_start(t_philo *philosophers, t_philo_data philo, pthread_mutex_t *forks, t_waiter *waiter)
+{
+	int				i;
+	long long		first_think;
+	struct timeval	actual_tv;
+
+	i = 0;
+	while (i < philo.number_of_philo)
+	{
+		printf("Se crea el filosofo numero %d\n", i + 1);
+		philosophers[i].id = i + 1;
+		gettimeofday(&actual_tv, NULL);
+		first_think = actual_tv.tv_sec * 1000 + actual_tv.tv_usec / 1000;
+		//printf("el tiempo para empezar a pensar es: %lld\n", first_think);
+		philosophers[i].data = philo;
+		philosophers[i].time_last_eat = first_think;
+		philosophers[i].left_fork = &forks[i];
+		if (i == (philo.number_of_philo - 1))
+			philosophers[i].right_fork = &forks[0];
+		else
+			philosophers[i].right_fork = &forks[i + 1];
+		philosophers[i].state = 0;
+		philosophers[i].is_alive = 1;
+		philosophers[i].waiter = waiter;
+		pthread_create(&philosophers[i].thread, NULL, routine, &philosophers[i]);
+		i++;
+	}
+}
+
+void	forks_start(pthread_mutex_t *forks, int n)
+{
+	int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		pthread_mutex_init(&forks[i], NULL);
+		i++;
+	}
+}
+
 void	philosophers(t_philo_data philo)
 {
 	t_philo	*philosophers;
+	pthread_mutex_t *forks;
+	t_waiter	waiter;
+	int	n;
 
-	philosophers = (t_philo *) malloc (sizeof t_philo * philo->number_of_philo);
+	n = philo.number_of_philo;
+	philosophers = (t_philo *) malloc (sizeof (t_philo) * n);
+	forks = (pthread_mutex_t *) malloc (sizeof (pthread_mutex_t) * n);
+	forks_start(forks, n);
+	waiter.id_next_eat = 1;
+	philosophers_start(philosophers, philo, forks, &waiter);
 }
 
-void	complete_philo_data(int argc, void **argv, t_philo_data *philo)
+void	complete_philo_data(int argc, char **argv, t_philo_data *philo)
 {
 	philo->number_of_philo = ft_atoi(argv[1]);
 	philo->time_to_die = ft_atoi(argv[2]);
@@ -79,10 +325,9 @@ void	complete_philo_data(int argc, void **argv, t_philo_data *philo)
 
 int	main(int argc, char **argv)
 {
-	int	number_to_eat;
-	philo_data_t philo;
+	t_philo_data philo;
 
-	printf("La cantidad de argumentos es: %d\n", argc);
+	//printf("La cantidad de argumentos es: %d\n", argc);
 	if (argc < 5 || argc > 6)
 	{
 		write(2, "Wrong number of arguments.\n", 27);
