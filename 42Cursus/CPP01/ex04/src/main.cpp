53ab6d4e3c5bac05f37ae598f2293e2ec566857e
Sergio Mejia Assmus <smejia-a@car11s6.42barcelona.com>
2026-06-08 18:35:42 +0200

    CPP01 v1.2

--- 42Cursus/CPP01/ex04/src/main.cpp
+++ 42Cursus/CPP01/ex04/src/main.cpp
@@ -2,10 +2,19 @@
 
 int	main(int argc, char *argv[])
 {
+	std::string	s1;
+
     if (argc != 4) {
         std::cerr << "Error. Needed 3 arguments: <filename> <string 1> <string 2>" << std::endl;
         return 1;
     }
+
+	s1 = argv[2];
+	if (s1.empty()) {
+		std::cerr << "Error. <string 1> can't be empty" << std::endl;
+		return 1;
+	}
+
     FileReplacer replacer(argv[1], argv[2], argv[3]);
 
     if(replacer.process() == false)
