3f836eb1e0e248e3442f241870a5614d06eb84a2
Sergio Mejia Assmus <smejia-a@car6s5.42barcelona.com>
2026-06-13 14:06:14 +0200

    CPP02 v0.4

--- 42Cursus/CPP02/ex02/src/Fixed.cpp
+++ 42Cursus/CPP02/ex02/src/Fixed.cpp
@@ -1,18 +1,14 @@
 #include "Fixed.hpp"
 
 Fixed::Fixed() {
-    std::cout << "Default constructor called" << std::endl;
+    this->_numberValue = 0;
 }
 
 Fixed::Fixed(const Fixed& other) {
-    std::cout << "Copy constructor called" << std::endl;
-
     *this = other;
 }
 
 Fixed& Fixed::operator= (const Fixed& other) {
-    std::cout << "Copy assignment operator called" << std::endl;
-
     if (this != &other) {
         this->_numberValue = other._numberValue;
     }
@@ -20,31 +16,22 @@ Fixed& Fixed::operator= (const Fixed& other) {
 }
 
 Fixed::~Fixed() {
-    std::cout << "Destructor called" << std::endl;
 }
 
 int Fixed::getRawBits(void) const {
-    std::cout << "getRawBits member function called" << std::endl;
-
     return _numberValue;
 }
 
 void Fixed::setRawBits(int const raw) {
-    std::cout << "setRawBits member function called" << std::endl;
-
     _numberValue = raw;
     return;
 }
 
 Fixed::Fixed(const int externalParameter) {
-    std::cout << "Int constructor called" << std::endl;
-
     _numberValue = externalParameter << _fractionalBits;
 }
 
 Fixed::Fixed(const float externalParameter) {
-    std::cout << "Float constructor called" << std::endl;
-
     _numberValue = roundf(externalParameter * (1 << _fractionalBits));
 }
 
@@ -62,31 +49,77 @@ std::ostream& operator<< (std::ostream& os, const Fixed& obj) {
         return os;
 };
 
-Fixed operator+ (const Fixed& other) const {
+Fixed Fixed::operator+ (const Fixed& other) const {
     Fixed result;
     result.setRawBits(this->_numberValue + other._numberValue);
     return result;
 };
 
-Fixed operator- (const Fixed& other) const {
+Fixed Fixed::operator- (const Fixed& other) const {
     Fixed result;
     result.setRawBits(this->_numberValue - other._numberValue);
     return result;
 };
 
-Fixed operator* (const Fixed& other) const {
-    Fixed result;
-    result.setRawBits(((long)(this->_numberValue) - (long)(other._numberValue)) >> _fractionalBits);
+Fixed Fixed::operator* (const Fixed& other) const {
+    Fixed   result;
+    long    temp;
+    temp = (long)(this->_numberValue) * (long)(other._numberValue);
+    result.setRawBits((int)(temp >> _fractionalBits));
     return result;
-}
-Fixed operator/ (const Fixed& other) const
-/*Increment/Decrement*/
-Fixed& operator++(void);
-Fixed operator++(int);
-Fixed& operator--(void);
-Fixed operator--(int)
-/*Overloaded Member Functions*/
-static Fixed& min(Fixed& first, Fixed& second);
-static const Fixed& min(const Fixed& first, const Fixed& second);
-static Fixed& max(Fixed& first, Fixed& second);
-static const Fixed& max(const Fixed& first, const Fixed& second;
+};
+
+
+Fixed Fixed::operator/ (const Fixed& other) const {
+    Fixed   result;;
+    long    temp;
+    temp = (long)(this->_numberValue) << _fractionalBits;
+    result.setRawBits((int)(temp / other._numberValue));
+    return result;
+};
+
+Fixed& Fixed::operator++(void) {
+    this->_numberValue++;
+    return *this;
+};
+
+Fixed Fixed::operator++(int) {
+    Fixed b(*this);
+    this->_numberValue++;
+    return b;
+};
+
+Fixed& Fixed::operator--(void) {
+    this->_numberValue--;
+    return *this;
+};
+
+Fixed Fixed::operator--(int) {
+    Fixed b(*this);
+    this->_numberValue--;
+    return b;
+};
+
+Fixed& Fixed::min(Fixed& first, Fixed& second) {
+    if (first._numberValue > second._numberValue)
+        return second;
+    return first;
+};
+
+const Fixed& Fixed::min(const Fixed& first, const Fixed& second) {
+    if (first._numberValue > second._numberValue)
+        return second;
+    return first;
+};
+
+Fixed& Fixed::max(Fixed& first, Fixed& second) {
+    if (first._numberValue < second._numberValue)
+        return second;
+    return first;
+};
+
+const Fixed& Fixed::max(const Fixed& first, const Fixed& second) {
+    if (first._numberValue < second._numberValue)
+        return second;
+    return first;
+};
